d6504fc83238e6b9a0c6f2f3b76f04d17fe8a6f4
minlwin <lwin.zawmin@gmail.com>
2016-02-17 18:49:38 +0630

    Update Servlet Tutorial 3

--- servlet-tut3/src/com/jdc/serv/redirect/RedirectFrom.java
+++ servlet-tut3/src/com/jdc/serv/redirect/RedirectFrom.java
@@ -12,7 +12,7 @@ public class RedirectFrom extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		response.sendRedirect("redirect-to.jsp?message=Hello From Servlet");
 	}
 
 }
